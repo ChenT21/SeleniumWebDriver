db94e0bb4320f918b1ff964fd4bdddde9d88f08d
ChenT21 <you@example.com>
2023-05-13 17:38:40 -0600

    Sesion 2

--- src/main/java/Poms/GooglePage.java
+++ src/main/java/Poms/GooglePage.java
@@ -1,5 +1,8 @@
 package Poms;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +11,9 @@ import org.openqa.selenium.WebElement;
 public class GooglePage {
 	WebDriver driver;
 	String searchBarId = "APjFqb";
-	String Result = "//h3[text()='Nintendo - Wikipedia']";
-	String text = "mw-page-title-main";
+	String Result = "//h3[text()='Wikipedia']";
+	String text = "//h3[@class='LC20lb MBeuO DKV0Md']";
+	String parrafo = "//*[@id=\"mw-content-text\"]/div[1]/p";
 
 	public GooglePage(WebDriver driver) {
 		super();
@@ -29,5 +33,32 @@ public class GooglePage {
 	public void printText() {
 		WebElement fr = driver.findElement(By.className(text));
 		System.out.println(fr.getText());
+	}
+	public void clickResultById(int result) {
+		List <WebElement> list = driver.findElements(By.xpath(text));
+		list.get(result).click();
+		System.out.println(list.size());
+	}
+	public void printTitle() {
+		System.out.println(driver.getTitle());
+	}
+	public void printParrafo() {
+		
+		List <WebElement> list = driver.findElements(By.xpath(parrafo));
+				
+		for(int i = 3; i >= 0; i--) {
+			System.out.println(list.get(i).getText());
+			
+		}
+		
+	}
+	
+	public void printResults() {
+		List <WebElement> list = driver.findElements(By.xpath(text));
+		
+		for(WebElement element: list) {
+			System.out.println(element.getText());
+		}
+		
 	}
 }
---
+++ src/main/java/scripts/Sesion2.java
@@ -0,0 +1,45 @@
+package scripts;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import Poms.GooglePage;
+
+public class Sesion2 {
+private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver(options);	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("https:/www.google.com");
+		GooglePage gp = new GooglePage(driver);
+		Thread.sleep(3000);
+		gp.typeOnSearchBar("Wikipedia");
+		Thread.sleep(1000);
+		//busqueda google
+		//gp.clickResult();
+		//gp.printText();
+		
+		gp.clickResultById(1);
+		//gp.printTitle();
+		//gp.printResults();
+		gp.printParrafo();
+		
+	}
+	
+	@After
+	public void teardown() {
+		
+		//driver.quit();
+	}
+}
